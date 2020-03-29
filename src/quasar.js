99f413ecce7514ffdaa9b46b80b47c771ec18838
Bassem <bassem.ayman1997@gmail.com>
2020-03-29 22:20:40 +0200

    frontend initialized using quasar

---
+++ src/quasar.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+
+import "./styles/quasar.scss";
+import "quasar/dist/quasar.ie.polyfills";
+import iconSet from "quasar/icon-set/fontawesome-v5.js";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
+import "@quasar/extras/material-icons-round/material-icons-round.css";
+import "@quasar/extras/material-icons-sharp/material-icons-sharp.css";
+import "@quasar/extras/fontawesome-v5/fontawesome-v5.css";
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel
+} from "quasar";
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel
+  },
+  directives: {},
+  plugins: {},
+  iconSet: iconSet
+});
