0537f9c2d3eeb02614e9f6a39cfa8dca52da423f
Bassem <bassem.ayman1997@gmail.com>
2020-05-03 06:42:32 +0200

    restored config

---
+++ config/setup.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    secretOrKey: process.env.SECRET,
+    email_bot: process.env.email_bot,
+    email_host: process.env.email_host,
+    email_user: process.env.email_user,
+    email_pass: process.env.email_pass,
+    email_port: process.env.email_port
+}
