822b2167b56706df0f3c1725c01cad4dc6097d88
Bassem <bassem.ayman1997@gmail.com>
2020-05-09 23:19:16 +0200

    edit

--- config/knexfile.js
+++ config/knexfile.js
@@ -16,7 +16,6 @@ module.exports = {
       charset: 'utf-8',
       timezone: 'utc'
     },
-
     pool: {
       min: 2,
       max: 10
@@ -32,7 +31,7 @@ module.exports = {
     // debug: true
   },
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_CONNECTION_URL+'?ssl=true',
     pool: {
       min: 2, // TODO might want to increase but based on the instance
