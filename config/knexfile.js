afaacbfe21fc98873de6fb9055f548d640a94d43
Bassem <bassem.ayman1997@gmail.com>
2020-05-09 23:28:38 +0200

    changed env variables

--- config/knexfile.js
+++ config/knexfile.js
@@ -32,7 +32,16 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_CONNECTION_URL+'?ssl=true',
+    // connection: process.env.DATABASE_CONNECTION_URL+'?ssl=true',
+    connection:{
+      database: process.env.DB_NAME1,
+      host: process.env.DB_HOST1,
+      port: process.env.DB_PORT1,
+      user: process.env.DB_USER1,
+      password: process.env.DB_PASSWORD1,
+      charset: 'utf-8',
+      timezone: 'utc'
+    },
     pool: {
       min: 2, // TODO might want to increase but based on the instance
       max: 10
