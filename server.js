5a8261dd9df29bd839bf280251c5e8fb877c2d5e
Bassem <bassem.ayman1997@gmail.com>
2020-05-10 01:30:04 +0200

    test

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 /* eslint-disable */
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(express.static(__dirname + '/dist/'))
-app.get(/.*/, function (req, res) {
-    res.sendfile(__dirname + '/dist/index.html')
-})
-app.listen(port)
-console.log(`App Running On Port ${port}`)
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
